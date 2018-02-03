8801a584467e01f900deea1f494cfefacfd80e2c
MrOStanMan <lucaskondrackyj@gmail.com>
2018-02-03 00:38:54 -0800

    completed Golf

---
+++ application/controllers/Golf.php
@@ -0,0 +1,19 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Tango
+ *
+ * @author Lucas
+ */
+class Golf extends Application {
+    public function index() 
+     {
+         $this->show(6);
+     }
+}
